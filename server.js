8f00c900ebd48c010bd90414142988ba02bdc7b4
Shalabyn <nadia.shalaby@nrc-cnrc.gc.ca>
2020-01-08 11:26:39 -0500

    added user functionality and socket development

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const io = require('socket.io')(3000)
+const io = require('socket.io')(3000);
+
+const users = {}
 
 io.on('connection', socket => {
-    socket.emit('chat-message', 'hello world')
-})
+    socket.on('new-user', name =>{
+        users[socket.id] = name
+        socket.broadcast.emit('user-connected')
+    })
+    socket.emit('chat-message', 'hello world');
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', message)
+    })
+});
